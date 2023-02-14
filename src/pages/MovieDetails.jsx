a7ded0af1e18c88c1170f9c36ac79ee87ef5ec1f
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-02-14 21:04:01 +0200

    лінк повернення з картки фільму не сприймає стилі класу і тягне стилі з Layout.module.css
    1) лінк повернення з картки фільму не сприймає стилі класу і тягне стилі з Layout.module.css; якщо обгорнути кнопкою - кнопка також не сприймає стилі класу;
    2) не працює коректно запит на бекенд пошуку фільмів за словом
    
    2)

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,14 +1,26 @@
-import { Outlet } from 'react-router-dom';
-import css from './PagesStyles.module.css';
+import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
+
 import Container from 'components/Container';
 import MovieCard from 'components/MovieCard';
 import MovieAddInfo from 'components/MovieAddInfo/MovieAddInfo';
+import css from './PagesStyles.module.css';
+import '../index.css';
 
 const MovieDetails = () => {
-    return (
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? "movies";
+
+  return (
     <>
       <Container>
-        <button type='button' style={{ marginLeft: '20px' }} className={css.backButton}>Go back</button>
+        {/* -------------------------------------  */}
+      {/* НЕ ПРАЦЮЮТЬ СТИЛІ ЛІНКА ПОВЕРНЕННЯ НА ПОПЕРЕДНЮ СТОРІНКУ */}
+        <NavLink to={backLinkHref} className={css.backLink}>Go back</NavLink>
+
+        <button type='button' style={{ marginLeft: '20px' }} className={css.backButton}>
+          <Link to={backLinkHref} className={css.backLink}>Go back</Link>
+        </button>
+      {/* --------------------------------------  */}
 
         <MovieCard/>
       
